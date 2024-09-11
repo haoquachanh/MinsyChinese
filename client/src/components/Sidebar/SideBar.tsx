2a28b2f31aec809740ed842d30461d79cf8dbc9f
Hao Quach <anhhaomnmm@gmail.com>
2024-09-11 15:24:34 +0700

    chore: fix err build

--- client/src/components/Sidebar/SideBar.tsx
+++ client/src/components/Sidebar/SideBar.tsx
@@ -1,13 +1,20 @@
 /* eslint-disable @next/next/no-img-element */
-'use client';
-import Link from 'next/link';
-import { navLinks, socialLinks } from '@/utils/navLink';
-import { Icon, SocialIcon } from '../Icons';
+"use client";
+import Link from "next/link";
+import { navLinks, socialLinks } from "@/utils/navLink";
+import { Icon, SocialIcon } from "../Icons";
 
 export default function SideBar() {
   return (
-    <div className="z-40 h-full drawer-side" style={{ scrollBehavior: 'smooth', scrollPaddingTop: '5rem' }}>
-      <label htmlFor="drawer" className="drawer-overlay" aria-label="Close menu" />
+    <div
+      className="z-40 h-full drawer-side"
+      style={{ scrollBehavior: "smooth", scrollPaddingTop: "5rem" }}
+    >
+      <label
+        htmlFor="drawer"
+        className="drawer-overlay"
+        aria-label="Close menu"
+      />
       <aside className="fixed flex flex-col border-e-2 border-spacing-8 bg-base-100 py-4 border-black/10 w-80 h-screen min-h-screen">
         <div
           data-sveltekit-preload-data=""
@@ -20,7 +27,11 @@ export default function SideBar() {
             className="flex justify-center align-middle"
             data-svelte-h="svelte-nce89e"
           >
-            <img src="/LaTEn.png" className="!py-1 w-auto h-24" alt="Logo"></img>
+            <img
+              src="/LaTEn.png"
+              className="!py-1 w-auto h-24"
+              alt="Logo"
+            ></img>
           </Link>
         </div>
         <div className="h-4" />
@@ -36,13 +47,21 @@ export default function SideBar() {
             ))}
           </ul>
           <div className="px-4 menu flex flex-row justify-center space-x-5 mb-5">
-            <SocialIcon kind="mail" href={`mailto:${socialLinks.email}`} size={5} />
-            <SocialIcon kind="github" href={socialLinks.github} size={5} />
+            <SocialIcon
+              kind="mail"
+              href={`mailto:${socialLinks.email}`}
+              size={5}
+            />
+            {/* <SocialIcon kind="github" href={socialLinks.github} size={5} /> */}
             <SocialIcon kind="facebook" href={socialLinks.facebook} size={5} />
             <SocialIcon kind="youtube" href={socialLinks.youtube} size={5} />
             <SocialIcon kind="linkedin" href={socialLinks.linkedin} size={5} />
             <SocialIcon kind="twitter" href={socialLinks.twitter} size={5} />
-            <SocialIcon kind="instagram" href={socialLinks.instagram} size={5} />
+            <SocialIcon
+              kind="instagram"
+              href={socialLinks.instagram}
+              size={5}
+            />
             <SocialIcon kind="threads" href={socialLinks.threads} size={5} />
           </div>
         </div>
